81a4bc05d574036264764f270b5caaafc0218a5b
Laura Planas Simón <lplanas97@gmail.com>
2021-06-01 01:14:03 +0200

    Added base structure and elements to first page

--- app.R
+++ app.R
@@ -1,19 +1,118 @@
 library(plotly)
+library(DT)
 
 library(shiny)
 library(shinydashboard)
+library(shinyWidgets)
 library(shinydashboardPlus)
 
 source("data.R")
 
 
 # Define UI 
-ui <- fluidPage(
+
+
+sidebar <- sidebarMenu(
+    menuItem("World indicators", tabName="page1", icon=icon("globe-americas")),
+    menuItem("Country analysis", tabName="page2", icon=icon("flag")),
+    menuItem("Correlated indicators", tabName="page3", icon=icon("calendar")),
+    menuItem("About", tabName="about", icon=icon("info-circle"))
     
-    selectInput("year", "Year", choices=c("All", getYears())),
-    selectInput("country", "Country", choices=c("All", getCountries())),
-    textOutput('testText')
+)
+
+
+page1_body <- tabItem(tabName = "page1",
+                      
+                      column(width = 8,
+                             
+                             box(title="Filters",
+                                 width = NULL,
+                                 
+                                 fluidRow(
+                                     column(width = 4,
+                                         selectInput("indicator", "Indicator", choices=getYears())
+                                     ),
+                                     
+                                     column(width = 4,
+                                        selectInput("year", "Year", choices=getYears())
+                                     ),
+                                     
+                                     column(width = 4,
+                                        selectInput("region", "Region", choices=c("All", getRegions()))
+                                     )
+                                 )
+                                
+                                 
+                                 
+                                 
+                             ),
+                             
+                             box(title="Worldwide indicators",
+                                 width = NULL,
+                                 plotlyOutput("map")
+                             )
+                             
+                     ),
+                      
+                      column(width = 4,
+
+                          box(title="Table",
+                              width = NULL,
+                              dataTableOutput("indicators_table")
+                          )
+                          
+                      )
+                      
+                      
+)
+
+page2_body <- tabItem(tabName = "page2",
+                      h2("Page 2"),
+                      selectInput("year", "Year", choices=getYears()),
+                      selectInput("country", "Country", choices=c("All", getCountries())),
+                      textOutput('testText')
+                      
+)
+
+
+
+
+
+body <- tabItems(
+        page1_body,
+        
+        page2_body,
+        
+        tabItem(tabName="page3",
+                h2("Page 3")
+        ),
+        
+        tabItem(tabName="about",
+                h2("About")
+            
+        )
+)
     
+
+
+
+
+
+
+
+ui <- dashboardPage(
+    
+    header=dashboardHeader(
+        title = tagList(
+            span(class = "logo-lg", "Happiness Analysis"), 
+            icon("smile"))
+    ),
+    
+    sidebar=dashboardSidebar(
+        sidebar
+    ),
+    body=dashboardBody(body),
+    footer=dashboardFooter()
 )
 
 
@@ -21,16 +120,20 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     filterData <- reactive({
-        data <- get_data()
+        data <- getData()
         
         # Filtering by year
-        if(input$year != "All"){
-            data <- data %>% filter(data$year == input$year)
-        }
+        data <- data %>% filter(data$year == input$year)
+        
         
         # Filtering by country
         if(input$country != "All"){
             data <- data %>% filter(data$Country.Name == input$country)
+        }
+        
+        # Filtering by region
+        if(input$region != "All"){
+            data <- data %>% filter(data$Region == input$region)
         }
         
         data
@@ -39,6 +142,27 @@ server <- function(input, output) {
     output$testText <- renderText({
         input$country
     })
+    
+    # Page 1 graphs ------------------------------------------------------------
+    # World Happiness Map
+    output$map <- renderPlotly({
+        data <- filterData()
+        
+        fig <- plot_ly(data, type='choropleth', locations=data$CountryCode, 
+                       z=data$Happiness.Score, text=data$Country.Name, 
+                       colors="Blues")
+        #fig <- fig %>% layout(autosize=F, height=300)
+        fig
+    })
+    
+    output$indicators_table <- renderDataTable({
+        data <- filterData()
+        table_data <- data %>% select("Country.Name", "Happiness.Score") %>% 
+            arrange(desc(Happiness.Score))
+        
+        datatable(table_data)
+    })
+    
     
     
 }
--- data.R
+++ data.R
@@ -1,20 +1,45 @@
 library(dplyr)
 
+getCountryCodes <- function(){
+  country_codes <- read.csv("data/Country.csv")
+  
+}
+
 getData <- function(){
   data <- read.csv("data/wdi_happiness_data.csv")
+  country_codes <- getCountryCodes()
+  merged <- merge(data, country_codes, by.x="Country.Name", by.y="TableName")
+  
+  return(merged)
 }
 
 getYears <- function(){
   # Returning unique years for filter
   data <- getData()
-  return(unique(data$year))
+  years <- arrange(data, year)$year
+  return(unique(years))
 }
 
-getCountries <- function(data){
+getCountries <- function(){
   # Returning unique countries for filter
   data <- getData()
-  return(unique(data$Country.Name))
+  countries <- arrange(data, Country.Name)$Country.Name
+  return(unique(countries))
 }
+
+getRegions <- function() {
+  data <- getData()
+  regions <- arrange(data, Region)$Region
+  return(unique(regions))
+}
+
+
+
+
+
+
+
+
 
 
 
